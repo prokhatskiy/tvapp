1849ee08c1a6dfc6f8cef1e3df5c5e5ade405fbe
Arem_Prokhatskyi <Arem_Prokhatskyi@epam.com>
2014-11-25 13:55:23 +0200

    added slideshow

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'EPAM TV' });
 });
+
+router.get('/slideshow', function(req, res) {
+  res.render('slideshow');
+});
+
 
 module.exports = router;
