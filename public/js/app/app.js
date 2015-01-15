0e8598c6e110e6ccd524fb7b7e72b7ed6767fb4c
Arem_Prokhatskyi <Arem_Prokhatskyi@epam.com>
2015-01-15 18:23:39 +0200

    changed links to services

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var tvapp = angular.module('tvapp', ['ngRoute', 'ui.date']);
+var tvapp = angular.module('tvapp', ['ui.sortable', 'ngRoute', 'ui.date', 'ngCookies']);
 
 tvapp
     .constant('APP_CONST', (function() {
@@ -11,6 +11,7 @@ tvapp
     .constant('ROUTES', (function() {
        return {
            ADMIN_ROOT: '/',
+           LOGIN_ROOT: '/login',
            ADMIN_EDIT: '/edit',
            ADMIN_ADD: '/add',
            ADMIN_TIMELINES: '/timeline'
@@ -18,11 +19,14 @@ tvapp
     }()))
     .constant('SERVICES', (function() {
         return {
-            ADMIN_SLIDES: '/fakeData/list.json',
-            GET_TIMELINE: '/fakeData/timeline.json',
-            POST_TIMELINE: '/fakeData/timeline.json',
-            GET_SLIDE: '/fakeData/slide.json',
-            POST_SLIDE: '/fakeData/slide.json',
+            CHECK_ACCESS: '/services/access',
+            ADMIN_SLIDES: '/services/slides',
+            GET_TIMELINE: '/services/slides',
+            POST_TIMELINE: '/services/slides',
+            LOGIN: '/services/login',
+            LOGOUT: '/services/logout',
+            GET_SLIDE: '/services/slide',
+            POST_SLIDE: '/services/slide/post',
             UPLOAD_IMG: '/fakeData/src.json'
         }
     }()))
@@ -34,6 +38,7 @@ tvapp
         var partialsPath = 'partials/';
 
         $routeProvider
+            .when(ROUTES.LOGIN_ROOT, {templateUrl: partialsPath + 'login.html', controller: 'loginCtrl'})
             .when(ROUTES.ADMIN_ROOT, {templateUrl: partialsPath + 'adminSlideList.html', controller: 'adminSlidesCtrl'})
             .when(ROUTES.ADMIN_EDIT + '/:id', {templateUrl: partialsPath + 'adminSlideEdit.html', controller: 'adminEditCtrl'})
             .when(ROUTES.ADMIN_ADD, {templateUrl: partialsPath + 'adminSlideEdit.html', controller: 'adminEditCtrl'})
